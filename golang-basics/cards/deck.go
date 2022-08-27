ca7ae3743120f86b2290f693b6f1b8092aa9bf5e
Jarod Christiansen <jarodchristiansen2@gmail.com>
2022-08-27 11:05:00 -0700

    intiial commit

---
+++ golang-basics/cards/deck.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// Create a new type of 'deck'
+// Whichi si a lsice of strings
+
+type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
